dc20b4c17e9e93d295927c627f373905806cbfef
BeauRussell <yourknightmares@gmail.com>
2025-01-04 20:31:23 -0600

    Set up air and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./internal/templates/**/*.html",
+		"./static/**/*.js"
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
+
